970eedda0d4efb82f9e8fcda729177ca70584379
eyadahmedatef <159515318+eyadahmedatef@users.noreply.github.com>
2024-07-23 14:35:34 +0300

    log

--- project14/script.js
+++ project14/script.js
@@ -1,32 +1,33 @@
-const emailInput = document.getElementById("emailInput");
-const passwordInput = document.getElementById("passwordInput");
+const userEmail = document.getElementById("userEmail");
+const userPassword = document.getElementById("userPassword");
+const mainDiv = document.getElementById("mainDiv");
 const errorText = document.getElementById("errorText");
+const spinnerDiv = document.getElementById("spinnerDiv");
 
-emailInput.addEventListener("input", function () {
-    if (emailInput.value == "") {
-        errorText.style.display = "none";
-    }
-});
 
-passwordInput.addEventListener("input", function () {
-    if (passwordInput.value == "") {
-        errorText.style.display = "none";
-    }
-});
 
 async function loginUser() {
-
-    const apiUrl = `https://tatbeqak.site/apps/tatbeqey/apps/projects/test_api/login.php?email=${emailInput.value}&password=${passwordInput.value}`;
-
+    mainDiv.style.display = "none";
+    spinnerDiv.style.display = "inline-block";
+    const apiUrl = https//tatbeqak.site/apps/tatbeqey/apps/projects/test_api/login.php?email=${userEmail.value}&password=${userPassword.value};
     const response = await fetch(apiUrl);
-
     const data = await response.json();
-
-    const status = data.status;
-
-    if (status == true) {
-        alert("true");
+    const result = data.status;
+    if(result == true){
+        location.href = "index2.html";
     } else {
         errorText.style.display = "block";
+    }
+    spinnerDiv.style.display = "none";
+    mainDiv.style.display = "block";
+}
+
+function checkLogin() {
+    if(userEmail.value == ""){
+        alert("Please enter valid email");
+    } else if(userPassword.value == ""){
+        alert("Plesse enter valid password");
+    } else {
+        loginUser();
     }
 }
